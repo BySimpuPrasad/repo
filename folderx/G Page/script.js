8c15881c052d1fef60fb7d9cee2343ac29cedf7d
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-02-18 21:34:48 +0530

    update

--- folderx/G Page/script.js
+++ folderx/G Page/script.js
@@ -1,16 +1,11 @@
 let settingBox = document.querySelector('.settingBox');
-let bgColor = "url('https://wallpaper.forfun.com/fetch/6c/6c5d2a23a84889f93b7a5cc8da42f122.jpeg?w=1470')";
 
 
-let wallpaperLocal = localStorage.getItem('betterwallpaper') || bgColor;
+let wallpaperLocal = localStorage.getItem('betterwallpaper') || 0;
 
 function setWallpaperAuto() {
-  if (wallpaperLocal == bgColor) {
-    document.body.style.background = wallpaperLocal;
-  }
-  else {
-    document.body.style.background = `url('${wallpaperLocal}')`;
-  }
+  if(wallpaperLocal == 0 ) return;
+  document.body.style.background = wallpaperLocal;
 }
 setWallpaperAuto();
 
@@ -32,12 +27,7 @@ function launchSetting() {
 function changeWallpaper() {
   let url = prompt('ENTER url OR -0');
   if (url == null) { return; }
-  if (url != "0") {
-    document.body.style.background = ` ${bgColor} url('${url}')`;
-  }
-  else {
-    document.body.style.background = bgColor;
-    url = bgColor;
-  }
+  if (url == "0") { localStorage.removeItem('betterwallpaper'); return;}
+  document.body.style.background = url;
   localStorage.setItem('betterwallpaper', url);
 }
