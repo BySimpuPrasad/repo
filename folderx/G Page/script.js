d2315bb8dbf9978e0779c93c431f631a75992dd3
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-02-18 18:50:14 +0530

    update

--- folderx/G Page/script.js
+++ folderx/G Page/script.js
@@ -1,4 +1,10 @@
-
-function searchData(e){
+function searchData(e) {
   window.location.href = `https://www.google.com/search?q=${search.value}`
+}
+
+function checkEnter(event) {
+  console.log(event.key);
+  if(event.key == "Enter"){
+    searchData();
+  }
 }
