539ab990c5626eddf30037e4ed76973e35332dfe
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-02-18 22:24:40 +0530

    update

--- folderx/G Page/script.js
+++ folderx/G Page/script.js
@@ -1,10 +1,14 @@
 let settingBox = document.querySelector('.settingBox');
-
+let cssroot = document.querySelector(':root');
 
 let wallpaperLocal = localStorage.getItem('betterwallpaper') || 0;
+let colorBG = localStorage.getItem('color1') || "#ffffff20";
+let colortxts = localStorage.getItem('color2') || "#fff";
 
 function setWallpaperAuto() {
-  if(wallpaperLocal == 0 ) return;
+  cssroot.style.setProperty('--lightColor', colorBG);
+  cssroot.style.setProperty('--colortxt', colortxts);
+  if (wallpaperLocal == 0) return;
   document.body.style.background = wallpaperLocal;
 }
 setWallpaperAuto();
@@ -27,8 +31,21 @@ function launchSetting() {
 function changeWallpaper() {
   let url = prompt(`ENTER url('___') OR -0`);
   if (url == null) { return; }
-  if (url == "0") { localStorage.removeItem('betterwallpaper'); return;}
+  if (url == "0") { localStorage.removeItem('betterwallpaper'); return; }
   document.body.style.background = url;
   localStorage.setItem('betterwallpaper', url);
   alert('road the page to see changes!')
+}
+
+function setColor(e) {
+  if (e == 1) {
+    let color = prompt('BG COLOR');
+    cssroot.style.setProperty('--lightColor', color);
+    localStorage.setItem('color1', color);
+  }
+  else if (e == 2) {
+    let color = prompt('TEXT COLOR');
+    cssroot.style.setProperty('--colortxt', color);
+    localStorage.setItem('color2', color);
+  }
 }
