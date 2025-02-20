7036b2f86bf0cf696fe13825adc7f13f55947f7c
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-02-20 18:54:26 +0530

    update

--- folderx/better_search/script.js
+++ folderx/better_search/script.js
@@ -1,10 +1,29 @@
 // GETTING ELEMENTS
 let link_list = document.getElementById('link_list');
 let settingBar = document.querySelector('.settingBar');
+let searchBar = document.getElementById('search-input');
 
 
+//Showing Setting Bar
+function showSetting() {
+  settingBar.classList.toggle('show');
+}
 
-//Adding new links
+
+//Searching Data
+function searchData(e) {
+  window.location.href = `https://www.google.com/search?q=${searchBar.value}`
+}
+
+function checkEnter(event) {
+  console.log(event.key);
+  if (event.key == "Enter") {
+    searchData();
+  }
+}
+
+
+//Adding Shortcut
 function addLinks() {
   let product;
   let name = prompt('site name:');
@@ -15,7 +34,13 @@ function addLinks() {
   product = `<span href=""><a href="${url}">${name}</a><i class="fa fa-close" onclick="this.parentElement.remove();"></i></span>`;
   link_list.innerHTML += product;
 }
-//Showing Setting Bar
-function showSetting(){
-  settingBar.classList.toggle('show');
+
+
+//Remove Shortcuts
+function removeShortcuts() {
+  link_list.classList.add('on');
+}
+
+function linkListDone(){
+  link_list.classList.remove('on');
 }
