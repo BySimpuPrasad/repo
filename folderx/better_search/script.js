bf3e0bcb841316d8a098dee274629663047545e0
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-02-20 19:23:08 +0530

    update

--- folderx/better_search/script.js
+++ folderx/better_search/script.js
@@ -1,8 +1,29 @@
+// GETTING FROM LOCAL STORAGE
+let body_background = localStorage.getItem('local_background');
+
+
 // GETTING ELEMENTS
 let link_list = document.getElementById('link_list');
 let settingBar = document.querySelector('.settingBar');
 let searchBar = document.getElementById('search-input');
 
+
+//Auto implement locals
+function implementLocals() {
+
+  //set background
+  if (body_background === null) {}
+  else { document.body.style.background = body_background; }
+
+
+}
+
+implementLocals();
+
+// Reload the alert
+function reloadTheAlert() {
+  alert('reload the page to see changes!');
+}
 
 //Showing Setting Bar
 function showSetting() {
@@ -41,6 +62,17 @@ function removeShortcuts() {
   link_list.classList.add('on');
 }
 
-function linkListDone(){
+function linkListDone() {
   link_list.classList.remove('on');
+}
+
+//Changing Background
+function backgroundChange() {
+  let url = prompt("set url('__'), color or default");
+  if (url == null) { return; }
+  else if (url == "default") { localStorage.removeItem('local_background'); reloadTheAlert();}
+  else {
+    localStorage.setItem('local_background', url);
+    reloadTheAlert();
+  }
 }
