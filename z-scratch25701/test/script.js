f977e21ddd219a70465e585c9aad183c94c4868b
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-03-09 11:46:45 +0530

    update

--- z-scratch25701/test/script.js
+++ z-scratch25701/test/script.js
@@ -2,9 +2,9 @@ function fetchAndGet() {
   fetch('https://bysimpuprasad.github.io/repo/fetchData/fetch.json')
     .then(response => response.json())
     .then(data => {
-      const func = new Function('data', data.step1);
-      console.log(func(data));
+      const func = new Function('data', data.toolBox);
+      func(data);
     });
 }
 
-//fetchAndGet();
+fetchAndGet();
