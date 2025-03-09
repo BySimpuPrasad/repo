c4a8906589878443045531e6111572909f228d0a
BySimpuPrasad <kumarsimpupd@gmail.com>
2025-03-09 10:30:33 +0530

    update

--- z-scratch25701/test/script.js
+++ z-scratch25701/test/script.js
@@ -2,7 +2,7 @@ function fetchAndGet() {
   fetch('https://bysimpuprasad.github.io/repo/fetchData/fetch.json')
     .then(response => response.json())
     .then(data => {
-      const func = new Function('data', data.greet);
+      const func = new Function('data', data.step1);
       console.log(func(data));
     });
 }
